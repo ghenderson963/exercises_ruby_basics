845e63d369cd952d71f0199999c02f846e897266
Gary Henderson <ghenderson963@gmail.com>
2021-02-11 21:30:47 -0800

    finished loop1 exercises

--- loops1/loops1.rb
+++ loops1/loops1.rb
@@ -32,3 +32,57 @@ loop do
   answer = gets.chomp
   break if answer == 'yes'
 end
+
+# Modify the code below so "Hello!" is printed 5 times.
+say_hello = true
+i = 0
+while say_hello
+  i += 1
+  puts 'Hello!'
+  say_hello = false if i ==5 
+end
+
+# Using the given code, print 5 random numbers between 0 and 99 
+
+
+numbers = (1..99).to_a
+i = 0
+while i < 5
+  puts numbers.sample
+  i += 1
+end
+
+num = []
+while num.size < 5
+  num << rand(100)
+end
+puts '====='
+puts num
+
+# modify the given code so it counts up from 1 to 10 rather than down.
+count = 1
+
+until count == 11
+  puts count
+  count += 1
+end
+
+# Given the array of several numbers below, use an until loop to print each number.
+numbers = [7,9,13,25,18]
+i = 0
+until numbers.size == i
+  puts numbers[i]
+  i += 1
+end
+
+# modify the code so that it only outputs i if i is an odd number
+for i in 1..100
+  puts i if i.odd?
+end
+
+# given the following array of names, use a for loop to greet each friend.
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+for friend in friends 
+  puts "Hi, #{friend}"
+end
