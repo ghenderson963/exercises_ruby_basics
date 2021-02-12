5f3e684ff27ec7f501876932fa3bf9028e28c08c
Gary Henderson <ghenderson963@gmail.com>
2021-02-11 20:34:32 -0800

    half way through loop1

--- loops1/loops1.rb
+++ loops1/loops1.rb
@@ -16,3 +16,19 @@ loop do
 end
 
 puts 'This is outside all loops' 
+
+# modify the following loop so it iterates 5 times instead of just once
+iterations = 1
+
+loop do 
+  puts "Number of iterations = #{iterations}"
+  iterations += 1
+  break if iterations > 5
+end
+
+# Modify the code below so the loop stops iterating when the user inputs 'yes'
+loop do 
+  puts 'Should I stop looping?'
+  answer = gets.chomp
+  break if answer == 'yes'
+end
