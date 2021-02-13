b09f5908f32e25a7e696763aea5e0c8fdd8ff927
Gary Henderson <ghenderson963@gmail.com>
2021-02-12 21:34:04 -0800

    added notes on methods used

--- loops2/loops2.rb
+++ loops2/loops2.rb
@@ -6,3 +6,16 @@ loop do
   count.odd? ? (puts "#{count} is odd!") : (puts "#{count} is even!")
   count += 1
 end
+
+# Modify the following code so that the loop stops if number is equal to or between 0 and 10
+loop do
+  number = rand(100)
+  puts number
+  break if number >= 0 && number <= 10
+end
+
+loop do 
+  number = rand(100)
+  puts number
+  break if number.between?(0,10)
+end
