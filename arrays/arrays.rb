6ae1d74d6fa54aee1d7acb838e8d24796d552d02
Gary Henderson <ghenderson963@gmail.com>
2021-03-12 20:49:46 -0800

    Finished hashes

--- arrays/arrays.rb
+++ arrays/arrays.rb
@@ -56,3 +56,5 @@ array1 = [1,5,9]
 array2 = [1,9,5]
 
 puts array1 == array2
+
+
--- hashes/hashes.rb
+++ hashes/hashes.rb
@@ -28,3 +28,60 @@ numbers = {
 
 numbers.each { |k,v| puts "A #{k} number is #{v}."}
 
+# Use map to iterate over the numbers hash below and return an array containing each number
+# divided by 2.  Assign the returned array to a variable named half_numbers and print its
+# value using p.
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+half_numbers = numbers.map { |k,v| v / 2 }
+p half_numbers
+
+# Use select to iterate over the numbers array below and return a hash containing only key-value
+# pairs where the value is less than 25.  Assign the returned hash to a variable named low_lnumbers
+# and print its value using p.
+
+numbers = {
+  high: 100,
+  medium: 50,
+  low: 10
+}
+
+low_numbers = numbers.select { |k,v| v < 25 }
+p low_numbers
+
+# Modify the code below to use the distructive version of select.
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+low_numbers = numbers.select! do |key, value|
+                value < 25
+              end
+
+p low_numbers
+p numbers 
+
+# Create a nested hash using the data supplied.
+
+vehicles = {
+  car:   {type: "sedan", color: "blue", year: 2003}, 
+  truck: {type: "pickup", color: "red", year: 1998}
+}
+p vehicles
+
+# Rewrite car as a nested array containing the same key-value pairs.
+car = {
+  type:  'sedan',
+  color: 'blue',
+  year:    2003
+}
+car = [[:type, 'sedan'],[:color,'blue'],[:year, 2003]]
+p car
